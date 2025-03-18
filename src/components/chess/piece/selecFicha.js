53cf10f10c9467bfc040e74a25f308f364673631
Setfer <andres88lacarolina@gmail.com>
2025-03-18 21:55:51 +0100

    18/03

--- src/components/chess/piece/selecFicha.js
+++ src/components/chess/piece/selecFicha.js
@@ -1,19 +1,61 @@
-export const selecFicha= (id, chess, dispatchChess, dispatchBoard)=>{
-  const piece = chess.pieces.find((piece)=> piece.id ===id)
-  if (!piece || chess.turno !== piece.color ) return
+export const selecFicha = (id, chess, dispatchChess, dispatchBoard) => {
+  const piece = chess.pieces.find((piece) => piece.id === id)
+  if (!piece || chess.turno !== piece.color) return
   dispatchChess({
-    type:"SELECT_FICHA",
-    payload:{
+    type: 'SELECT_FICHA',
+    payload: {
       id
     }
   })
-  dispatchBoard({
-    type: "ACTIVE_VALID",
-    payload:{
-      rows: piece.nextRows.map((row)=>row+piece.row),
-      cols :piece.nextCols.map((col)=>col+piece.col),
-      isValid:true,
+
+  let payload
+  //si la ficha es peon
+  if (piece.type === 'peon') {
+    //si la ficha tiene alguna diagonal para comer
+    const piezasParaComer = chess.pieces.filter(
+      (pieceComer) =>
+        pieceComer.color !== piece.color &&
+        ((piece.color === 'white' &&
+          pieceComer.row === piece.row + 1 &&
+          (pieceComer.col === piece.col + 1 ||
+            pieceComer.col === piece.col - 1)) ||
+          (piece.color === 'black' &&
+            pieceComer.row === piece.row - 1 &&
+            (pieceComer.col === piece.col + 1 ||
+              pieceComer.col === piece.col - 1)))
+    )
+
+    payload = {
+      
+    }
+
+
+    //si es el primer moviento.
+    const primerMovimiento = piece.row === 1 || piece.row === 6 ? true : false
+
+    //si el peon estan en la posicion inial
+    if (primerMovimiento) {
+      payload = {
+        rows:
+          piece.color === 'white'
+            ? [piece.row + 1, piece.row + 2]
+            : [piece.row - 1, piece.row - 2],
+        cols: piece.nextCols.map((col) => col + piece.col),
+        isValid: true,
+        color: piece.color
+      }
+    }
+  }  else {
+    payload = {
+      rows: piece.nextRows.map((row) => row + piece.row),
+      cols: piece.nextCols.map((col) => col + piece.col),
+      isValid: true,
       color: piece.color
     }
+  }
+
+  dispatchBoard({
+    type: 'ACTIVE_VALID',
+    payload
   })
 }
